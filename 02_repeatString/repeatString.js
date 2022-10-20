048b93dd2e6973868894bc1398efc5139e5527c2
HossBigft <20mingot@gmail.com>
2022-10-20 19:27:20 +0400

    Add repeat string exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+let returnString=string;
+if(num<0){
+    return 'ERROR';
+}
+if(string==''|num===0){
+    return ''
+}
+let counter =0;
+while (counter<num-1){
+    returnString=returnString.concat(string);
+    counter++;
+};
+return returnString;
 };
 
 // Do not edit below this line
