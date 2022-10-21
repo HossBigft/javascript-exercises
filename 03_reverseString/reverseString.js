794242fc2b071be38f199f1d947a7c352dc464f7
HossBigft <20mingot@gmail.com>
2022-10-21 18:07:27 +0400

    Add reverse string exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+if(str===''){
+    return '';
+}
+return reverseString(str.substring(1)) + str.charAt(0);
 };
 
 // Do not edit below this line
