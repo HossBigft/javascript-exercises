a338f71f1bab55f67aa935c06954fa47f1e855e5
HossBigft <20mingot@gmail.com>
2022-10-21 19:18:20 +0400

    Add remove from array exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, item) {
+ let args = Array.from(arguments);
+args.forEach(item=>{
+    let index = array.indexOf(item);
+    if (index !== -1) {
+        array.splice(index, 1);
+       }
 
+    });
+return array;
 };
-
+let arr = [1,2,3,5];
+arr=removeFromArray(arr, 3);
 // Do not edit below this line
 module.exports = removeFromArray;
