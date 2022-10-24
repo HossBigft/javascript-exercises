368bb4922400c8193532c83e35e22094e36b3eba
HossBigft <20mingot@gmail.com>
2022-10-24 17:54:45 +0400

    Add sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+let sum=0;
+if(start<0||end<0||typeof(start)!='number'||typeof(end)!='number'){
+    return "ERROR"
+}
+let currentIndex=start;
+let endIndex=end;
+if(currentIndex>endIndex){
+    currentIndex=end;
+    endIndex=start;
+}
+for(currentIndex;currentIndex<=endIndex;currentIndex++){
+    sum+=currentIndex;
+}
+return sum;
 };
 
+sumAll(1,4);
 // Do not edit below this line
 module.exports = sumAll;
