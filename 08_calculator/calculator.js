592819dbe68c9e68c155eb68c49277257545e522
HossBigft <20mingot@gmail.com>
2022-10-28 19:14:06 +0400

    Add calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(first, second) {
+  return  first + second;
 };
 
-const subtract = function() {
-	
+const subtract = function(first, second) {
+	return first-second;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  let sum = 0;
+	for(let i =0;i<numbers.length; i++){
+    sum+=numbers[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(number) {
+  let multiplication = 1;
+	for(let i =0;i<number.length; i++){
+    multiplication*=number[i];
+  }
+  return multiplication;
 };
 
-const power = function() {
-	
+const power = function(number, power) {
+	let result = 1;
+  for (let i = 0;i<power;i++){
+    result*=number;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let factorial = 1;
+	for(let i = 1;i<=number;i++){
+    factorial*=i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
