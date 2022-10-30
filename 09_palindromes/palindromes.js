16e11aeb72a8ff6fda076cfa55dcfd1daf643f0e
HossBigft <20mingot@gmail.com>
2022-10-30 13:49:49 +0400

    Add palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
-
+function revertString(string){
+    if(string===''){
+        return '';
+    }
+    return revertString(string.substring(1)) + string.charAt(0);
+}
+function removeSpecialChars(str) {
+    return str.replace(/(?!\w|\s)./g, '')
+      .replace(/\s+/g, ' ')
+      .replace(/^(\s*)([\W\w]*)(\b\s*$)/g, '$2')
+      .replace(/\s/g, "");
+  }
+const palindromes = function (string) {
+    let comparedString= removeSpecialChars(string).toLowerCase();
+    if(comparedString===revertString(comparedString)){
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
